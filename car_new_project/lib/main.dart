08a3bdd058208cf42515d9f4f1756b13bb8a2df1
ishaque <ishaque@mobikasa.com>
2023-01-19 15:56:18 +0530

    initial commit

---
+++ car_new_project/lib/main.dart
@@ -0,0 +1,54 @@
+import 'package:flutter/material.dart';
+import 'package:webview_flutter/webview_flutter.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: const MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key});
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  late WebViewController controller;
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.white,
+      ),
+      body: Stack(
+        children: [
+          WebView(
+            initialUrl: 'http://dev.caroneview.com',
+            backgroundColor:Color(0xff252379) ,
+            javascriptMode: JavascriptMode.unrestricted,
+            onPageFinished: (_) {},
+            onWebViewCreated: (WebViewController webViewController) {
+              controller = webViewController;
+              webViewController.clearCache();
+            },
+          ),
+        ],
+      ),
+    );
+  }
+}
